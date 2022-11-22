24699afa170de2cd3f6781dc6ede38579cd32abc
xyamix <xyamix1337@gmail.com>
2022-11-23 01:20:50 +0300

    ...

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -1,74 +1,86 @@
 import { Button, Col, Menu, Row } from "antd";
 import React, { useState } from "react";
 import {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    ContainerOutlined,
-    UnorderedListOutlined,
-    PlusOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  ContainerOutlined,
+  UnorderedListOutlined,
+  PlusOutlined,
+  UserOutlined
 } from "@ant-design/icons";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function getItem(label, key, icon, children, type) {
-    return {
-        key,
-        icon,
-        children,
-        label,
-        type,
-    };
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
 }
 
 const items = [
-    getItem("Создать", "1", <PlusOutlined />),
-    getItem("Наборы", "2", <UnorderedListOutlined />),
-    getItem("Option 3", "3", <ContainerOutlined />),
+  getItem("Профиль", "profile", <UserOutlined />),
+  getItem("-", "-", undefined, undefined, "divider"),
+  getItem("Создать", "create", <PlusOutlined />),
+  getItem("Наборы", "sets", <UnorderedListOutlined />),
+  getItem("Option 3", "4", <ContainerOutlined />),
 ];
 
 export const MainMenu = () => {
-    const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+  
+  const navigate = useNavigate();
+  
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
 
-    const toggleCollapsed = () => {
-        setCollapsed(!collapsed);
-    };
+  const onMenuItemClicked = (item) => {
+    let key = item.key;
+    if(key === "profile")
+      navigate("profile");
+  };
 
-    return (
-        <Row style={{width: "100%"}}>
-            <Col span={3}>
-                <div
-                    style={{
-                        width: 210,
-                    }}
-                    className="App-menu_wrapper"
-                >
-                    <Button
-                        type="primary"
-                        onClick={toggleCollapsed}
-                        style={{
-                            marginBottom: 16,
-                        }}
-                        shape="circle"
-                    >
-                        {collapsed ? (
-                            <MenuUnfoldOutlined />
-                        ) : (
-                            <MenuFoldOutlined />
-                        )}
-                    </Button>
-                    <Menu
-                        className="App-menu"
-                        defaultSelectedKeys={["1"]}
-                        defaultOpenKeys={["sub1"]}
-                        mode="inline"
-                        theme="lighth"
-                        inlineCollapsed={collapsed}
-                        items={items}
-                    />
-                </div>
-            </Col>
-            <Col span={20} style={{width: "100%"}}>
-                <Outlet />
-            </Col>
-        </Row>
-    );
+  return (
+    <Row style={{ width: "100%" }}>
+      <Col span={3}>
+        <div
+          style={{
+            width: 210,
+          }}
+          className="App-menu_wrapper"
+        >
+          <Button
+            type="primary"
+            onClick={toggleCollapsed}
+            style={{
+              marginBottom: 16,
+            }}
+            shape="circle"
+          >
+            {collapsed ? (
+              <MenuUnfoldOutlined />
+            ) : (
+              <MenuFoldOutlined />
+            )}
+          </Button>
+          <Menu
+            className="App-menu"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            onClick={(item) => onMenuItemClicked(item)}
+            mode="inline"
+            theme="lighth"
+            inlineCollapsed={collapsed}
+            items={items}
+          />
+        </div>
+      </Col>
+      <Col span={20} style={{ width: "100%" }}>
+        <Outlet />
+      </Col>
+    </Row>
+  );
 };
